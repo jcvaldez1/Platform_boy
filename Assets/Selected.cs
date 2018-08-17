a500bb687feb39719d67830567c100376ea72696
jcvaldez1 <jcvaldez1@up.edu.ph>
2018-08-17 17:56:37 +0800

    initial commit

---
+++ Assets/Selected.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Selected : MonoBehaviour {
+
+	// BE SURE TO OPTIMIZE BY PUTTING NECESSARY CONSTANTS IN CONSTANTS CLASS
+	public int index;
+	private GameObject[] elements;
+	private GameObject consts;
+	private Color RED = Color.red;
+	private Color GREEN = Color.green;
+	private GameObject platform;
+	private string the_platform;
+
+
+	void Start () {
+		consts = GameObject.Find("Constants");
+		the_platform = "Straight_Boy";
+		platform = GameObject.Find(the_platform);
+		foreach (Renderer r in platform.GetComponentsInChildren<Renderer>()) {
+			r.material.SetColor("_Color", RED);
+		}
+	}
+
+	void Update () {
+		Constants refScript = consts.GetComponent<Constants>();
+
+		if (refScript.CURRENT_SELECTED_BLOCK_INDEX == index) {
+			foreach (Renderer r in platform.GetComponentsInChildren<Renderer>()) {
+				r.material.SetColor("_Color", GREEN);
+			}
+			gameObject.GetComponent<Movement>().enabled = true;
+			gameObject.GetComponent<ShapeShift>().enabled = true;
+
+		} else {
+			//foreach (Transform element in platform) {
+			foreach (Renderer r in platform.GetComponentsInChildren<Renderer>()) {
+				//rend = element.GetComponent<Renderer>();
+				r.material.SetColor("_Color", RED);
+			}
+			gameObject.GetComponent<Movement>().enabled = false;
+			gameObject.GetComponent<ShapeShift>().enabled = false;
+		
+		}
+	
+	}
+
+
+}
---
+++ Assets/ShapeShift.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ShapeShift : MonoBehaviour {
+
+	private float waittime = 0.1f;
+	private string this_name = "Straight_Boy";
+	private bool pos_init_flag = false;
+	private GameObject collider_0;
+	private GameObject collider_1;
+	private string COLLIDER_0_NAME;
+	private string COLLIDER_1_NAME;
+	private Rotation_Checker COLLIDER_0;
+	private Rotation_Checker COLLIDER_1;
+
+	void Start () {
+
+		COLLIDER_0_NAME = "Checker_Boy_0";
+		COLLIDER_1_NAME = "Checker_Boy_1";
+		collider_0 = GameObject.Find (COLLIDER_0_NAME);
+		collider_1 = GameObject.Find (COLLIDER_1_NAME);
+		COLLIDER_0 = collider_0.GetComponent<Rotation_Checker> ();
+		COLLIDER_1 = collider_1.GetComponent<Rotation_Checker> ();
+	}
+
+	void Update () {
+		
+		if (Input.GetKeyDown ("space") && !COLLIDER_0.IS_COLLIDED  && !COLLIDER_1.IS_COLLIDED   ) {
+			Invoke("Flippo" , waittime);
+		}
+	}
+
+	void Flippo () {
+
+		// WOULD BE NICE TO PUT A CHECKER HERE TO SEE IF OBJECT COULD ROTATE OR NOT.
+		transform.Rotate (0, 0, 90, Space.Self);
+
+	}
+
+}
